f60fc29d62fad505f05181f67427c18ab9152254
Urmil Vora <urmilvora910@gmail.com>
2022-12-03 16:42:54 +0530

    LogIn Page

--- LYEV/myapp/views.py
+++ LYEV/myapp/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
+
 def index(request):
     template = loader.get_template('index.html') # our template
+    return HttpResponse(template.render())  # rendering template in HttpResponse
+    
+def logIn(request):
+    template = loader.get_template('logIn.html') # our template
     return HttpResponse(template.render())  # rendering template in HttpResponse
