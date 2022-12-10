5fae6998a116f2702889089df7a986b37d08bf31
Urmil Vora <urmilvora910@gmail.com>
2022-12-10 23:47:32 +0530

    CSRF token & POST variables identified

--- LYEV/myapp/views.py
+++ LYEV/myapp/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 # importing loading from django template
 from django.template import loader
-# Create your views here.
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     template = loader.get_template('index.html') # our template
     return HttpResponse(template.render())  # rendering template in HttpResponse
     
 def signIn(request):
-    template = loader.get_template('signIn.html') # our template
-    return HttpResponse(template.render())  # rendering template in HttpResponse
+    if request.method == 'POST':
+        email = request.POST["emailId"]
+        password = request.POST["password"]
+        # do something
+    else:
+        template = loader.get_template('signIn.html') # our template
+        return HttpResponse(template.render())  # rendering template in HttpResponse
     
+@csrf_exempt
 def signUp(request):
-    template = loader.get_template('signUp.html') # our template
-    return HttpResponse(template.render())  # rendering template in HttpResponse
+    if request.method == 'POST':
+        email = request.POST["emailId"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+        trckingConf = request.POST["confVal"]
+        # do something
+    else:
+        template = loader.get_template('signUp.html') # our template
+        return HttpResponse(template.render())  # rendering template in HttpResponse
